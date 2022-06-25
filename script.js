7cbe89ead51828cac03d5755934a4be29fe1413c
Akindeko Samuel <samuelakindeko@yahoo.com>
2022-06-25 14:56:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const name="Akindeko Samuel Jesutofunmi";
+const height= "5.6ft";
+const country= "Nigeria";
+
+alert("\n NAME: " + name + " \n \n \n" +
+"HEIGHT: " + height + " \n \n \n" +
+"COUNTRY: " + country);
+
+console.log(name + " \n \n" + height + "\n \n \n" + country);
